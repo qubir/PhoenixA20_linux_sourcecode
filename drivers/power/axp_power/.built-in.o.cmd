19ccbda83c84b972ec0c232726173f7f7870979d
anichips <jixinhui@jixinhui-System-Product-Name.(none)>
2013-11-13 20:23:17 +0800

    init linux source code repo

---
+++ drivers/power/axp_power/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/power/axp_power/built-in.o :=  arm-linux-gnueabi-ld -EL    -r -o drivers/power/axp_power/built-in.o drivers/power/axp_power/axp-mfd.o drivers/power/axp_power/axp20-board.o drivers/power/axp_power/axp20-regu.o drivers/power/axp_power/axp20-sply-cou.o drivers/power/axp_power/axp20-gpio.o drivers/power/axp_power/virtual20.o drivers/power/axp_power/virtual20_dev.o 
