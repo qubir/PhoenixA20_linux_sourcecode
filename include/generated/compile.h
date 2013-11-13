f8a196d5ec80ba613cdb6fc5ded453ea72b65217
anichips <jixinhui@jixinhui-System-Product-Name.(none)>
2013-11-13 10:25:47 +0800

    init linux source code repo

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 25 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#25 SMP PREEMPT Tue Nov 12 20:49:47 CST 2013"
+#define LINUX_COMPILE_BY "jixinhui"
+#define LINUX_COMPILE_HOST "jixinhui-System-Product-Name"
+#define LINUX_COMPILER "gcc version 4.6.3 20120201 (prerelease) (crosstool-NG linaro-1.13.1-2012.02-20120222 - Linaro GCC 2012.02) "
